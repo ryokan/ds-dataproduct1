c125b701c158b50d87a47a0012534145eece5c29
Yoshihiro Ueda <yoshihiroueda@gmail.com>
2015-09-25 18:59:14 +0900

    first

---
+++ server.R
@@ -0,0 +1,42 @@
+## Data Product 1
+library(shinyapps)
+require(shiny)
+
+library(datasets)
+data(mtcars)
+newmpg <- mean(mtcars$mpg)
+mtcars2 <- mtcars
+datanum <- dim(mtcars)[1]
+mtcars2[datanum+1,] <- 0 # for the new data
+mtcars2[, "color"] <- 3  # green
+mtcars2[datanum+1, "color"] <- 4 # Blue
+
+fit2<- lm(mpg ~ wt + cyl + hp + am, data=mtcars)
+
+shinyServer(
+  function(input, output) {
+     output$wt <- renderPrint({input$wt})
+     output$cyl <- renderPrint({input$cyl})
+     output$hp <- renderPrint({input$hp})
+     output$am <- renderPrint({ifelse ((input$am == 1), "Manual", "Automatic")})
+     
+     output$mpg <- renderText({
+       new[c("wt", "cyl", "hp", "am")] <- c(as.numeric({input$wt}),
+                                            as.numeric({input$cyl}),
+                                            as.numeric({input$hp}),
+                                            as.numeric({input$am}))
+       newmpg <- predict(fit2, newdata=new)
+       newmpg})
+     
+    output$graph1 <- renderPlot({
+      mtcars2[datanum+1, c("wt", "cyl", "hp", "am")] <- c(as.numeric({input$wt}),
+                                                          as.numeric({input$cyl}),
+                                                          as.numeric({input$hp}),
+                                                          as.numeric({input$am}))
+      mtcars2[datanum+1, "mpg"] <- predict(fit2, newdata=mtcars2[datanum+1,])
+      plot(mtcars2$wt, mtcars2$mpg, col = mtcars2$color,
+           xlab = "Weight", ylab = "MPG", 
+           main = "Relationship between car weight and MPG")
+    })
+  }
+)
---
+++ ui.R
@@ -0,0 +1,30 @@
+## data product 1 
+
+shinyUI(pageWithSidebar(
+  headerPanel("Predicting MPG for an automobile model"),
+  sidebarPanel(
+    h3("Input parameters"),
+    sliderInput('wt', 'Weight (ib/1000)', value = 3, min = 1, max = 5, step = 0.005),
+    numericInput('cyl', "Number of cylinders", 4,  min = 4, max = 8, step = 2),
+    radioButtons('am', "Transmission", choices = c("Automatic" = 0, "Manual" = 1)),
+    textInput('hp', "HousePower (60 ~ 330)", value = "200"),
+    h4("Usage Guide"), 
+p("This application predicts MPG (Miles/Gallon) for a given automobile model.  
+      Please input the parameters above and the predicted MPG will be shown in the right
+  panel.  Scatter plot in the right bottom is the relationship between MPG and the weight, 
+  shich is the most significant parameter to MPG.  The blue spot among green spots is
+  the one you specified.")
+  ),
+  mainPanel(
+    h3("Prediction Result"),
+    h4("Input"),
+   p("Weight = "), textOutput('wt'),
+    p("Number of cylinders = "), textOutput('cyl'),
+     p("Horsepower = "), textOutput('hp'),
+     p("Transmission:"), textOutput('am'),
+     h4("Prediction"),
+ p("MPG = "), textOutput("mpg"),
+    plotOutput('graph1')
+  )
+  
+))
